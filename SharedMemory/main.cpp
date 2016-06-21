4de60955d31a6316d6fd9dcd658b8a2b0fe583c1
Ubuntu <nir.neeraj@gmail.com>
2016-06-21 10:43:27 +0000

    shared memory example

--- SharedMemory/main.cpp
+++ SharedMemory/main.cpp
@@ -1,9 +1,18 @@
+#include <boost/interprocess/shared_memory_object.hpp>
 #include <iostream>
 
-using namespace std;
+using namespace boost::interprocess;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+
+  shared_memory_object shdmem{open_or_create, "Boost", read_write};
+  shdmem.truncate(1024);
+  std::cout << shdmem.get_name() << '\n';
+  offset_t size;
+
+  if (shdmem.get_size(size))
+    std::cout << size << '\n';
+
+
 }
